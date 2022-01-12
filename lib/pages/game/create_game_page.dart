0e32b7a107ec9fd17371e1ec6909a986172cfc03
Sebastian Celis <sebastian.celis@flexlog.de>
2022-01-12 16:33:52 +0100

    Add main counter page

--- lib/pages/game/create_game_page.dart
+++ lib/pages/game/create_game_page.dart
@@ -1,25 +1,82 @@
+import 'package:doppelkopf/pages/game/game_page.dart';
+import 'package:doppelkopf/services/transitions/transition_utils.dart';
 import 'package:flutter/material.dart';
 
 class CreateGamePage extends StatelessWidget {
   const CreateGamePage({Key? key}) : super(key: key);
 
+  bool attemptGameCreation() {
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
+    NameFields nameFieldWidgets = NameFields();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Create a new game"),
       ),
-      body: Column(
-        children: const [
-          Spacer(),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 25),
+            child: const Center(
+              child: Text(
+                "Please enter the names of the 4 players",
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: nameFieldWidgets,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
           Center(
-            child: Text("Please enter the names of the 4 players:"),
+            child: TextButton(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                child: Text(
+                  'Start game',
+                  style: TextStyle(fontSize: 16, color: Colors.grey.shade600),
+                ),
+              ),
+              onPressed: () => {
+                if (nameFieldWidgets.allFieldsAreValid())
+                  {
+                    Navigator.of(context)
+                        .pushReplacement(slideTransitionBuilder(
+                            GamePage(
+                              playerNames: nameFieldWidgets.getFieldValues(),
+                            ),
+                            SlideType.LEFT))
+                  }
+              },
+              style: ButtonStyle(
+                side: MaterialStateProperty.all(
+                  const BorderSide(
+                    color: Colors.black12,
+                    style: BorderStyle.solid,
+                  ),
+                ),
+                shape: MaterialStateProperty.all(
+                  const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                ),
+                shadowColor: MaterialStateProperty.all(Colors.white70),
+                foregroundColor: MaterialStateProperty.all(Colors.red),
+                elevation: MaterialStateProperty.all(5),
+                backgroundColor: MaterialStateProperty.all(Colors.white),
+              ),
+            ),
           ),
-          Padding(
-            padding: EdgeInsets.all(15.0),
-            child: NameFields(),
-          ),
-          Spacer(flex: 3)
         ],
       ),
     );
@@ -27,45 +84,83 @@ class CreateGamePage extends StatelessWidget {
 }
 
 class NameFields extends StatefulWidget {
-  const NameFields({Key? key}) : super(key: key);
+  NameFields({Key? key}) : super(key: key);
+
+  final fieldControllers = List.generate(4, (_) => TextEditingController());
 
   @override
   _NameFieldsState createState() => _NameFieldsState();
+
+  bool allFieldsAreValid() {
+    return fieldControllers
+        .map((e) => _isValidField(e.text))
+        .every((element) => element);
+  }
+
+  Set<String> getFieldValues() {
+    return fieldControllers.map((e) => e.text).toSet();
+  }
+
+  bool _isValidField(String field) {
+    return field.length >= 4 && field.length <= 15;
+  }
 }
 
 class _NameFieldsState extends State<NameFields> {
-  final playerIds = [1, 2, 3, 4];
+  final playerIds = [0, 1, 2, 3];
 
-  final controllers = List.filled(4, TextEditingController());
-  final focusNodes = List.filled(4, FocusNode());
+  final focusNodes = List.generate(4, (_) => FocusNode());
+
+  List errorTexts = List.filled(4, null);
 
   @override
   Widget build(BuildContext context) {
-    final children = playerIds
+    final textInputFields = playerIds
         .map((i) => Container(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          child: TextFormField(
-                controller: TextEditingController(),
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
+              padding: const EdgeInsets.symmetric(vertical: 6),
+              child: TextFormField(
+                controller: widget.fieldControllers.elementAt(i),
+                keyboardType: TextInputType.name,
+                textInputAction: i != playerIds.length
+                    ? TextInputAction.next
+                    : TextInputAction.done,
+                focusNode: focusNodes.elementAt(i),
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  labelText: 'Player ' + i.toString(),
-                  errorText: null,
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.amber)),
+                  labelText: 'Player ' + (i + 1).toString(),
+                  errorText: errorTexts.elementAt(i),
                   prefixIcon: const Icon(Icons.person),
                 ),
+                onChanged: (value) => {
+                  if (errorTexts.elementAt(i) != null)
+                    {
+                      if (widget._isValidField(value))
+                        {
+                          setState(() {
+                            errorTexts[i] = null;
+                          })
+                        }
+                    }
+                },
                 onFieldSubmitted: (value) {
-                  if(i == playerIds.length){
-                    return;
+                  dynamic _errorText;
+                  if (!widget._isValidField(value)) {
+                    _errorText =
+                        "Please enter a name between 4 and 15 characters!";
+                        focusNodes.elementAt(i).requestFocus();
                   }
-                  // focusNodes.elementAt(i).requestFocus();
+                  setState(() {
+                    errorTexts[i] = _errorText;
+                  });
                 },
               ),
-        ))
+            ))
         .toList();
 
-    return Column(children: children);
+    return Column(
+      children: textInputFields,
+    );
   }
 }
--- lib/pages/game/game_page.dart
+++ lib/pages/game/game_page.dart
@@ -1,15 +1,95 @@
 import 'package:flutter/material.dart';
 
 class GamePage extends StatelessWidget {
-  const GamePage({ Key? key }) : super(key: key);
+  final Set<String> playerNames;
+
+  const GamePage({Key? key, required this.playerNames}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> playerCards = playerNames
+        .map((playerName) => PlayerCard(
+              playerName: playerName,
+            ))
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Doppelkopf"),
       ),
-      body: const Center(child: Text("Game data..."),),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: playerCards,
+      ),
+    );
+  }
+}
+
+class PlayerCard extends StatefulWidget {
+  final String playerName;
+
+  const PlayerCard({Key? key, required this.playerName}) : super(key: key);
+
+  @override
+  _PlayerCardState createState() => _PlayerCardState();
+}
+
+class _PlayerCardState extends State<PlayerCard> {
+  int points = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
+    return Card(
+      child: SizedBox(
+        height: deviceHeight / 6,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  widget.playerName,
+                  style: const TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Row(
+                  children: [
+                    FloatingActionButton(
+                      child: const Icon(Icons.plus_one),
+                      onPressed: () => {
+                        setState(() {
+                          points++;
+                        })
+                      },
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    FloatingActionButton(
+                      child: const Icon(Icons.exposure_minus_1_rounded),
+                      onPressed: () => {
+                        setState(() {
+                          points--;
+                        })
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
+            Center(
+              child: Text(
+                points.toString(),
+                style: const TextStyle(
+                  fontSize: 32,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
